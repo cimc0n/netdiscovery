848d041d7bb49f2a705162406b9457eb5225d9ae
cimc0n <len4ik.vs@gmail.com>
2021-02-11 23:55:05 +0200

    Update trace.sh

--- trace.sh
+++ trace.sh
@@ -11,6 +11,19 @@ do
 done
 } 
 
+function Robtex
+{
+curl 'https://www.robtex.com/dns-lookup/'$1 > _robtex
+
+for value in {2,3,4,5,6,7,8,9,10,11,12}
+do
+ip=$(echo  $(cat _robtex) |  awk -F'<div><h3>Mail servers</h3></div>' '{print $2}' | awk -F'results shown' '{print $1}' |awk -F'dns-lookup/' '{print $'$value'}' |awk -F'">' '{print $1}')
+#ip=$(echo  $(cat _robtex) |  awk -F'<div><h3>IP numbers of the mail servers</h3></div>' '{print $2}' | awk -F'results shown' '{print $1}' |awk -F'ip-lookup/' '{print $'$value'}' |awk -F'">' '{print $1}')
+arrVar+=($ip.)
+done
+rm _robtex
+}
+
 function ReadTraceTargets
 {
  input="target.txt"
@@ -20,22 +33,26 @@ function ReadTraceTargets
   arrVar+=($line)
 else
   NsLookup $line
+  Robtex $line
 fi
 done < "$input"
 }
 
 function Trace
 {
-for value in "${arrVar[@]}"
+sorted_arrVar=($(echo "${arrVar[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
+for value in "${sorted_arrVar[@]}"
 do
-traceroute -m 3 -p25 $value >> $value.try
-echo '-------------------' >> $value.try
-traceroute -m 15 -I $value >> $value.try
+# traceroute -m 3 -p25 $value >> $value.try
+#echo '-------------------' >> $value.try
+#traceroute -m 15 -I $value >> $value.try
 echo $value
+
 done
-tar -cf outtrace.tar *.try
-rm *.try
+#tar -cf outtrace.tar *.try
+#rm *.try
 }
+
 
 echo "Start Program"
 ReadTraceTargets
