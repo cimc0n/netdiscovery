5e9e3bd9719c00d9358865397bbcd0b2a288f757
cimc0n <len4ik.vs@gmail.com>
2021-02-12 23:47:56 +0200

    Update RobtexMailServers.cs

--- RobtexMailServers.cs
+++ RobtexMailServers.cs
@@ -7,7 +7,7 @@ namespace RobtexSharp
     {
         static void Main(string[] args)
         {
-        new MyProgram().Robtex("ping.eu");
+         new MyProgram().Robtex("ping.eu");
         }
     }
     class MyProgram
@@ -19,14 +19,14 @@ namespace RobtexSharp
                 var Response = httpClient.GetStringAsync("https://www.robtex.com/dns-lookup/"+i);
                 if  (Response.Result.Contains("IP numbers of the mail servers"))
                 {
-                string Result = Response.Result.Split("IP numbers of the mail servers")[1];
-                Result = Result.Remove(Result.IndexOf("results shown."));
-                Regex ip = new Regex(@">(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})<");
-                MatchCollection _result = ip.Matches(Result);
-                foreach (Match bla in _result)  
-                {
+                 string Result = Response.Result.Split("IP numbers of the mail servers")[1];
+                 Result = Result.Remove(Result.IndexOf("results shown."));
+                 Regex ip = new Regex(@">(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})<");
+                 MatchCollection _result = ip.Matches(Result);
+                 foreach (Match bla in _result)  
+                 {
                     Console.WriteLine(bla.Groups[1].Value);                
-                }
+                 }
                 }
             }
         }
